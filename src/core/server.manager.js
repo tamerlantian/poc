84dabde0a0e34d753451e1b7ba4d9094a81cca6d
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2024-01-11 08:57:42 -0500

    feat: poc

--- src/core/server.manager.js
+++ src/core/server.manager.js
@@ -26,7 +26,7 @@ class ServerManager {
     try {
       console.log(` ${this._namespace}: Loading`);
 
-      this.#setupDependencies();
+      await this.#setupDependencies();
 
       this.#setupConsole();
 
@@ -73,10 +73,10 @@ class ServerManager {
     }
   }
 
-  #setupDependencies() {
+  async #setupDependencies() {
     const { DependenciesManager } = require('./dependencies.manager');
     this._dependenciesManager = new DependenciesManager(this._args);
-    this._dependenciesManager.setup();
+    await this._dependenciesManager.setup();
 
     this._dependenciesManager.core.add(
       this._dependenciesManager,
